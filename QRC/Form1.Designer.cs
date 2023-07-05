557dc97f623a2dcc287da4a5f2710029e3e9a33a
Bhaskar Shukla <bhaskarshukla002@gmail.com>
2023-07-05 16:29:59 +0530

    Add project files.

---
+++ QRC/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿using System.ComponentModel;
+using System.Drawing.Printing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace QRC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.drawBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rackLabel = new System.Windows.Forms.Label();
+            this.rackCB = new System.Windows.Forms.ComboBox();
+            this.rowLabel = new System.Windows.Forms.Label();
+            this.rowCB = new System.Windows.Forms.ComboBox();
+            this.spaceInputLabel = new System.Windows.Forms.Label();
+            this.spaceInput = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spaceInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 55);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1754, 1240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // drawBtn
+            // 
+            this.drawBtn.Location = new System.Drawing.Point(717, 8);
+            this.drawBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.drawBtn.Name = "drawBtn";
+            this.drawBtn.Size = new System.Drawing.Size(100, 34);
+            this.drawBtn.TabIndex = 1;
+            this.drawBtn.Text = "Draw";
+            this.drawBtn.UseVisualStyleBackColor = true;
+            this.drawBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.rackLabel);
+            this.panel1.Controls.Add(this.rackCB);
+            this.panel1.Controls.Add(this.rowLabel);
+            this.panel1.Controls.Add(this.rowCB);
+            this.panel1.Controls.Add(this.drawBtn);
+            this.panel1.Controls.Add(this.spaceInputLabel);
+            this.panel1.Controls.Add(this.spaceInput);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1756, 51);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // rackLabel
+            // 
+            this.rackLabel.AutoSize = true;
+            this.rackLabel.Location = new System.Drawing.Point(14, 15);
+            this.rackLabel.Name = "rackLabel";
+            this.rackLabel.Size = new System.Drawing.Size(48, 24);
+            this.rackLabel.TabIndex = 3;
+            this.rackLabel.Text = "Rack";
+            // 
+            // rackCB
+            // 
+            this.rackCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.rackCB.FormattingEnabled = true;
+            this.rackCB.Location = new System.Drawing.Point(67, 12);
+            this.rackCB.MaxDropDownItems = 10;
+            this.rackCB.Name = "rackCB";
+            this.rackCB.Size = new System.Drawing.Size(121, 32);
+            this.rackCB.TabIndex = 4;
+            // 
+            // rowLabel
+            // 
+            this.rowLabel.AutoSize = true;
+            this.rowLabel.Location = new System.Drawing.Point(232, 15);
+            this.rowLabel.Name = "rowLabel";
+            this.rowLabel.Size = new System.Drawing.Size(46, 24);
+            this.rowLabel.TabIndex = 5;
+            this.rowLabel.Text = "Row";
+            // 
+            // rowCB
+            // 
+            this.rowCB.FormattingEnabled = true;
+            this.rowCB.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y"});
+            this.rowCB.Location = new System.Drawing.Point(295, 12);
+            this.rowCB.Name = "rowCB";
+            this.rowCB.Size = new System.Drawing.Size(121, 32);
+            this.rowCB.TabIndex = 6;
+            // 
+            // spaceInputLabel
+            // 
+            this.spaceInputLabel.AutoSize = true;
+            this.spaceInputLabel.Location = new System.Drawing.Point(454, 15);
+            this.spaceInputLabel.Name = "spaceInputLabel";
+            this.spaceInputLabel.Size = new System.Drawing.Size(88, 24);
+            this.spaceInputLabel.TabIndex = 7;
+            this.spaceInputLabel.Text = "Gap(mm)";
+            // 
+            // spaceInput
+            // 
+            this.spaceInput.Location = new System.Drawing.Point(548, 11);
+            this.spaceInput.Name = "spaceInput";
+            this.spaceInput.Size = new System.Drawing.Size(100, 32);
+            this.spaceInput.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1364, 845);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "QR Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spaceInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private void InitializeRackComboBox()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                string number = i.ToString("D2"); // Format the number as a two-digit string with leading zeros
+                this.rackCB.Items.Add(number); // Add the number to the rackCB control
+            }
+        }
+        //private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        //{
+        //    // Perform time-consuming operation here
+        //}
+
+        //private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        //{
+        //    // Perform another time-consuming operation here
+        //}
+
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button drawBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label rackLabel;
+        private System.Windows.Forms.ComboBox rackCB;
+        private System.Windows.Forms.Label rowLabel;
+        private System.Windows.Forms.ComboBox rowCB;
+        private System.Windows.Forms.Label spaceInputLabel;
+        private System.Windows.Forms.NumericUpDown spaceInput;
+        //private System.Windows.Forms.ComboBox comboBox2;
+        //private System.Windows.Forms.Label label3;
+
+        
+    }
+    
+}
+
---
+++ QRC/Form1.cs
@@ -0,0 +1,131 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeRackComboBox();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gappixel = 0;
+            int ygappixel = 0;
+            string selectedRow;
+            string selectedRack;
+            int selectedSpace;
+            int qrQuantity;
+            
+            float mmpi = 25.4f;
+            int dpi = 150;
+            if (validate()) { 
+                selectedRow = rowCB.SelectedItem.ToString();
+                selectedRack = rackCB.SelectedItem.ToString();
+                selectedSpace = (int)spaceInput.Value;
+                qrQuantity = 50;
+                gappixel = (int)((selectedSpace * dpi) / 25.4);
+                ygappixel = (int)((7 * dpi) / 25.4);
+                int xpos = 0, ypos = 0;
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                Bitmap A4 = new Bitmap((int)(297 / mmpi * dpi), (int)( 210 / mmpi * dpi));
+                A4.SetResolution(dpi, dpi);
+                Graphics g = Graphics.FromImage(A4);
+                
+
+                for (int i = 0; i < qrQuantity; i++) {
+                    String currQRString = selectedRack + selectedRow + (i<10 ? "0" : "") + (i+1).ToString();
+                    Bitmap qrBitmap = getQRBitmap(qrGenerator, currQRString);
+                    g.DrawImage(qrBitmap, xpos, ypos);
+                    if (xpos + ((qrBitmap.Width + gappixel)*2) > A4.Width)
+                    {
+                        if (ypos + ((qrBitmap.Height + ygappixel) * 2) > A4.Height)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            ypos = ypos + qrBitmap.Height + ygappixel;
+                            xpos = 0;
+                        }
+                    }
+                    else
+                    {
+                        xpos = xpos + qrBitmap.Width + gappixel;
+                    }
+                }
+                pictureBox1.Image = A4;
+                //QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                //QRCodeData qrCodeData = qrGenerator.CreateQrCode("A001.", QRCodeGenerator.ECCLevel.Q);
+                //QRCode qrCode = new QRCode(qrCodeData);
+
+                //QRCodeData qrCodeData2 = qrGenerator.CreateQrCode("A002", QRCodeGenerator.ECCLevel.Q);
+                //QRCode qrCode2 = new QRCode(qrCodeData);
+
+                //Bitmap qr1 = qrCode.GetGraphic(2);
+                //Bitmap qr2 = qrCode2.GetGraphic(2);
+
+
+                //Bitmap bitmap = new Bitmap(qr1.Width * 20, qr1.Height);
+
+                //        using (Graphics g = Graphics.FromImage(bitmap))
+                //          {
+                //            g.DrawImage(qr1, 0, 0);
+                //              g.DrawImage(qr2, qr1.Width + gappixel, 0);
+
+                //                pictureBox1.Image = bitmap;
+                //}
+
+            }
+            
+
+
+        }
+
+        private Bitmap getQRBitmap(QRCodeGenerator qrGenerator,String qrCodeString) 
+        {
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrCodeString, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrBitmap = qrCode.GetGraphic(2);
+            return qrBitmap;
+        }
+
+        private Boolean validate()
+        {
+            if (rackCB.SelectedItem != null && rowCB.SelectedItem != null)
+            { 
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Please select all the required fields.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
+
+
